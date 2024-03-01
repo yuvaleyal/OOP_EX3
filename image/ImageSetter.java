9a8a104bc1f12a3ed397fb9adec3b954562ccd39
Yuval Eyal <uveyal@gmail.com>
2024-03-01 15:25:53 +0200

    mor eimae setter

--- image/ImageSetter.java
+++ image/ImageSetter.java
@@ -4,8 +4,18 @@ import java.awt.Color;
 
 public class ImageSetter {
     private Image image;
+    private int height;
+    private int width;
+    private int subImageSize;
+    private int imageResolution;
+
+    private static final double RED_MULTIPICATOR = 0.2126;
+    private static final double GREEN_MULTIPICATOR = 0.7152;
+    private static final double BLUE_MULTIPICATOR = 0.0722;
 
     public ImageSetter(Image img) {
+        height = getClosestPowerOfTwo(img.getHeight());
+        width = getClosestPowerOfTwo(img.getWidth());
         image = padImage(img);
     }
 
@@ -17,8 +27,6 @@ public class ImageSetter {
     private Image padImage(Image image) {
         int ogWidth = image.getWidth();
         int ogHeight = image.getHeight();
-        int width = getClosestPowerOfTwo(ogWidth);
-        int height = getClosestPowerOfTwo(ogHeight);
         Color[][] pixelArray = new Color[height][width];
         for (int row = 0; row < height; row++) {
             for (int col = 0; col < width; col++) {
@@ -36,8 +44,43 @@ public class ImageSetter {
         return new Image(pixelArray, width, height);
     }
 
-    // res is a power of 2
-    private void createSubImages(int res) {
+    /**
+     * this function assume that res <= width, height and that res is a power of 2.
+     * 
+     * @param res
+     *            number of sub-pictures in a row
+     */
+    public void updateResulotion(int res) {
+        subImageSize = width / res;
+        imageResolution = res;
+    }
+
+    private Image getSubImage(int x, int y) {
+        Color[][] pixelArray = new Color[subImageSize][subImageSize];
+        int firstCoordinateX = x * subImageSize;
+        int firstCoordinateY = y * subImageSize;
+        for (int row = 0; row < subImageSize; row++) {
+            for (int col = 0; col < subImageSize; col++) {
+                pixelArray[row][col] = image.getPixel(firstCoordinateX + row,
+                        firstCoordinateY + col);
+            }
+        }
+        return new Image(pixelArray, subImageSize, subImageSize);
+    }
+
+    private double getGrey(Color c) {
+        return c.getRed() * RED_MULTIPICATOR + c.getGreen() * GREEN_MULTIPICATOR
+                + c.getBlue() * BLUE_MULTIPICATOR;
+    }
+
+    private double getSubImageBrightness(int x, int y) {
+        Image subImnage = getSubImage(x, y);
+        double greySum = 0;
+        for (int row = 0; row < subImnage.getHeight(); row++) {
+            for (int col = 0; col < subImnage.getWidth(); col++) {
+                greySum += getGrey(subImnage.getPixel(row, col));
+            }
+        }
 
     }
 }

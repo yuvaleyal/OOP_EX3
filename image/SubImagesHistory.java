eca02fcec86687906bebac862de3b30c798793f7
Yuval Eyal <uveyal@gmail.com>
2024-03-02 20:33:58 +0200

    sub image history doc

--- image/SubImagesHistory.java
+++ image/SubImagesHistory.java
@@ -6,10 +6,22 @@ import image.ImageSetter.BrightnessMemento;
 public class SubImagesHistory {
     private TreeMap<Integer, BrightnessMemento> history;
 
+    /**
+     * Constructs a new SubImagesHistory instance to store the history of brightness
+     * mementos.
+     */
     public SubImagesHistory() {
         history = new TreeMap<Integer, BrightnessMemento>();
     }
 
+    /**
+     * Saves a brightness memento for a specific resolution in the history.
+     *
+     * @param resolution
+     *            The resolution of the sub-image.
+     * @param memento
+     *            The brightness memento to be saved.
+     */
     public void saveSubImage(int resolution, BrightnessMemento memento) {
         if (history.containsKey(resolution)) {
             return;
@@ -17,6 +29,14 @@ public class SubImagesHistory {
         history.put(resolution, memento);
     }
 
+    /**
+     * Retrieves the brightness memento for a specific resolution from the history.
+     *
+     * @param resolution
+     *            The resolution of the sub-image.
+     * @return The brightness memento corresponding to the specified resolution, or
+     *         null if not found.
+     */
     public BrightnessMemento getSubImageByResolution(int resolution) {
         if (history.containsKey(resolution)) {
             return history.get(resolution);
